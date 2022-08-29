942b0c50bac423f436bc0e22ef01b62d1b666f42
lengyangyu <admin@example.com>
2022-08-29 23:43:09 +0800

    fix(git): add git keymap

--- linux/git.sh
+++ linux/git.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 #====== git快捷命令 ======
-#显示所有分支
-gba
 #提交
 gaa
-gc
-gcam
 ggpush
 # 撤销对文件的修改
 grs file_name
@@ -16,22 +12,9 @@ glg
 glgg
 glo
 glol
-#全局修改核心编辑器--最终作用于~/.gitconfig文件
-git config --global core.editor nvim
 #同步repo依赖
 git submodule sync
 git submodule update --init --progress
-#修改提交注释并再次提交
-#或者创建新的提交代替旧的提交
-git commit --amend
-#查看全局配置
-git config --global --list
-#查看局部配置
-git config --list
-#切换到某一个分支
-gsw -c 分支名 # -c 创建
-#合并分支
-git merge origin/分支名
 #git-svn 并基
 gsr
 #git-svn 上传
@@ -39,8 +22,6 @@ gsb
 #代码比对
 gd
 git difftool -t nvimdiff
-#查看分支提交记录
-git reflog
 #
 #commit 撤销操作
 git reset --mixed HEAD~1 // 不删除空间改动代码，撤销commit和gaa，同时可以回退多个旧commit
@@ -53,14 +34,12 @@ gst
 gss
 gsb
 #拉取远程分支
-git checkout -b feature-branch origin/feature-branch    #检出远程的feature-branch分支到本地
+gcb feature-branch origin/feature-branch    #检出远程的feature-branch分支到本地
 #本地传建分支并推到远程
-git checkout -b feature-branch    #创建并切换到分支feature-branch
-git push origin feature-branch:feature-branch    #推送本地的feature-branch(冒号前面的)分支到远程origin的feature-branch(冒号后面的)分支(没有会自动创建)
+gcb feature-branch    #创建并切换到分支feature-branch
+gp origin feature-branch:feature-branch    #推送本地的feature-branch(冒号前面的)分支到远程origin的feature-branch(冒号后面的)分支(没有会自动创建)
 #删除远程分支
 gp origin --delete feature-branch
-#删除本地分支
-gb -D sub-branch
 #删除变基操作
 git rebase --abort
 #查看代码是谁修改的
@@ -70,7 +49,8 @@ git rebase --abort
 [c
 ]c
 dp
-## 拉取远程分支
+
+########################################## 拉取远程分支
 # 拉取远程的当前分支并合并
 ggpull
 # 拉取远程origin分支但不合并
@@ -80,7 +60,7 @@ gf branch-name
 # 拉取远程的所有分支但不合并
 gfa
 
-## 远程仓库管理
+########################################## 远程仓库管理
 # 查看远程信息
 grv
 # 增加远程仓库
@@ -90,7 +70,7 @@ grrm origin
 # 重命名远程仓库名
 grmv origin
 
-## 打标签
+######################################### 打标签
 # 为当前提交打标签
 g tag -a v1.0 -m "版本1.0"
 # 为历史提交打标签
@@ -103,4 +83,36 @@ gp origin -d v1.0
 gtv
 # 推送仓库和所有标签到远程仓库
 gpoat
+# 检出分支
+gco v1.0
+# 检出分支并修改提交
+gcb new-branch tag-name
 
+######################################### 分支管理
+#显示所有分支
+gba
+#删除本地分支
+gb -D sub-branch
+#切换到某一个分支
+gsw -c 分支名 # -c 创建
+#合并分支
+gm origin/分支名
+#查看重要的提交记录
+git reflog
+
+######################################### 提交记录
+# 配合nvim coc-conventional 插件规范提交
+gc
+# 非规范提交
+gcam
+#修改提交注释并再次提交
+#或者创建新的提交代替旧的提交
+git commit --amend
+
+######################################### 配置
+#全局修改核心编辑器--最终作用于~/.gitconfig文件
+git config --global core.editor nvim
+#查看全局配置
+git config --global --list
+#查看局部配置
+git config --local --list
